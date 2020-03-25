7b697d902a5479a14a5494c58e5c42ec6f66c07d
supattanachai <60215408+supattanachai@users.noreply.github.com>
2020-03-26 06:54:51 +0700

    Update line.php

--- line.php
+++ line.php
@@ -7,34 +7,6 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-//API Url
-$url = '(https://maker.ifttt.com/trigger/click_final/with/key/fF25pJ-v04BVZm7CRNJbhCXN49MzHkkrPRrH1HFgeDA';
-
-//Initiate cURL.
-$ch = curl_init($url);
-
-//The JSON data.
-$jsonData = array(
-    'Value1' => '169',
-    'Value2' => '55',
-    'Value3' => '16'
-);
-
-//Encode the array into JSON.
-$jsonDataEncoded = json_encode($jsonData);
-
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
-
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
-
-//Execute the request
-$result = curl_exec($ch);
-
 if ( sizeof($request_array['events']) > 0 )
 {
 
@@ -58,13 +30,11 @@ if ( sizeof($request_array['events']) > 0 )
 			$reply_message = 'ขณะนี้อยู่ที่ห้องเรียน IF-5T05 ...!!!';
 		}
 	   	else if(($text== "อยากทราบยอด COVID-19 ครับ")||($text== "อยากทราบยอดCOVID-19ครับ")||($text== "อยากทราบยอดCOVID-19")||($text== "COVID-19")){
-			$reply_message = 'https://maker.ifttt.com/trigger/click_final/with/key/fF25pJ-v04BVZm7CRNJbhCXN49MzHkkrPRrH1HFgeDA';
-				//$result; 
-				//$reply_message = 'รายงานสถานการณ์ ยอดผู้ติดเชื้อไวรัสโคโรนา 2019 (COVID-19) ในประเทศไทย"
-					//ผู้ป่วยสะสม     จำนวน 398,995 ราย
-					//ผู้เสียชีวิต	  จำนวน 17,365 ราย
-					//รักษาหาย      จำนวน 103,753 ราย
-					//ผู้รายงานข้อมูล: นายกานต์ เจริญจิตร';
+				$reply_message = 'รายงานสถานการณ์ ยอดผู้ติดเชื้อไวรัสโคโรนา 2019 (COVID-19) ในประเทศไทย"
+					ผู้ป่วยสะสม     จำนวน 398,995 ราย
+					ผู้เสียชีวิต	  จำนวน 17,365 ราย
+					รักษาหาย      จำนวน 103,753 ราย
+					ผู้รายงานข้อมูล: นายกานต์ เจริญจิตร';
 		}
 	   	else if(($text== "ข้อมูลส่วนตัวของผู้พัฒนาระบบ")||($text== "ผู้พัฒนาระบบ")||($text== "ข้อมูลส่วนตัวของผู้พัฒนา")||($text== "ข้อมูลผู้พัฒนา")){
 			$reply_message = 'ชื่อนายสุพัฒนชัย ปานสุรินทร์ อายุ 22ปี น้ำหนัก 75kg. สูง 151cm. ขนาดรองเท้าเบอร์ 39 ใช้หน่วย EUR';
